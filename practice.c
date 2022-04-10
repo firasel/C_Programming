562b4a4113a6a129731630de84df5d828b7fd4ee
firasel <mdrasel601390@gmail.com>
2022-04-10 23:38:30 +0600

    First day practice declaration & variable & output print

---
+++ practice.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int marks = 10;
+	marks = marks + 10;
+	printf("%d ", marks);
+
+	marks = marks - 50;
+	marks = 20 + 5;
+	marks = marks * 2;
+	printf("%d ", marks);
+
+    return 0;
+}
