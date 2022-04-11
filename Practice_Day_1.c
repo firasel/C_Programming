0f56c42b6947eb192af1fac7a99a12f367c4578d
firasel <mdrasel601390@gmail.com>
2022-04-11 22:06:27 +0600

    Practice day problem solved

---
+++ Practice_Day_1.c
@@ -0,0 +1,92 @@
+////Problem 1
+//#include <stdio.h>
+//
+//int main ()
+//{
+//    printf ("Testing...");
+//    printf ("....1");
+//    printf ("...2");
+//    printf ("..3");
+//    printf ("\n");
+//    return 0;
+//}
+
+
+////Problem 2
+//#include <stdio.h>
+//int main ()
+//{
+//    int sum;
+//    // COMPUTE RESULT
+//    sum = 25 + 37 - 19;
+//    /* DISPLAY RESULTS */
+//    printf("The answer is %d\n",sum);
+//
+//    return 0;
+//}
+
+
+////Problem 3
+//#include<stdio.h>
+//int main ()
+//{
+//    printf("\tHello\n");
+//    printf("\t nice\n");
+//    printf("\t   to\n");
+//    printf("\t meet\n");
+//    printf("\t  you\n");
+//}
+
+
+////Problem 4
+//#include<stdio.h>
+//int main ()
+//{
+//    char A$= 'K';
+//    printf("%c \n",A$);
+//
+//    return 0;
+//}
+
+
+////Problem 5
+//#include <stdio.h>
+//int main ()
+//{
+//int x, y;
+//scanf("%d %d", &x, &y);
+//printf("%d\n", x/y);
+//return 0;
+//}
+
+
+////Problem 6
+//#include<stdio.h>
+//int main(){
+//    int height;
+//    printf("Enter your height in inches: ");
+//    scanf("%d",&height);
+//    double height_calc = (double)height / 12;
+//    printf("Your height is %lf feet",height_calc);
+//
+//    return 0;
+//}
+
+
+////Problem 7
+//#include<stdio.h>
+//int main(){
+//    int height, width, area, perimeter;
+//
+//    printf("Enter height of rectangle: ");
+//    scanf("%d",&height);
+//    printf("Enter width of rectangle: ");
+//    scanf("%d",&width);
+//    perimeter = 2 * ( height + width );
+//    area = height * width;
+//
+//    printf("Area is: %d\n",area);
+//    printf("Perimeter is: %d\n",perimeter);
+//
+//    return 0;
+//}
