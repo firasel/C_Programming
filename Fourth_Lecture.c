e4c5f7db05aad348b170ec19cdb9d89ba7956c3d
firasel <mdrasel601390@gmail.com>
2022-04-23 23:42:18 +0600

    Number Guess game with while loop

--- Fourth_Lecture.c
+++ Fourth_Lecture.c
@@ -1,16 +1,28 @@
 #include<stdio.h>
+#include<stdbool.h>
 
 int main(){
-    int a, b;
-    scanf("%d %d", &a, &b);
+    srand(time(0));
+    int num = rand()%100 + 1;
 
-    while (a != 0) {
-        int rem = b%a;
-        b = a;
-        a = rem;
-        printf("%d %d\n", a, b);
+    int number_of_limit = 10;
+
+    while (number_of_limit>0) {
+        int inputNum;
+        scanf("%d",&inputNum);
+
+        if (inputNum == num) {
+            printf("You are right\n");
+            break;
+        } else if (inputNum > num) {
+            printf("Guess smaller\n");
+        } else {
+            printf("Guess larger\n");
+        }
+        number_of_limit--;
     }
-    printf("Gcd = %d\n",b);
+
+    number_of_limit == 0 && printf("You failed");
 
     return 0;
 }
