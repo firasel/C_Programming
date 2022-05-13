ffcdfd048c06223ea29635dfd3a3ef411da60ab8
firasel <mdrasel601390@gmail.com>
2022-05-14 01:33:19 +0600

    assignmen fully complete

--- LabAssignment_3.c
+++ LabAssignment_3.c
@@ -1,24 +1,7 @@
-#include<stdio.h>
-
-int main() {
-    int n,d;
-    scanf("%d %d",&n,&d);
-    int a[n-1];
-    for(int i=0; i<n; i++)
-        scanf("%d",&a[i]);
-
-    for(int i=0; i<d; i++){
-        int temp = a[0];
-        for(int j=0; j<(n-1);j++){
-            a[j] = a[j+1];
-        }
-        a[n-1] = temp;
+    #include<stdio.h>
+    int main()
+    {
+        char str[10] = { NULL };
+        printf("%d",str[9]==NULL ? 0 : 1);
+        return 0;
     }
-
-    for(int i=0; i<n; i++)
-        printf("%d ",a[i]);
-    printf("\n");
-
-
-    return 0;
-}
