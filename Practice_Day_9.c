bfea04cdc4b0ed9b55e25185bb61f82003c3f138
firasel <mdrasel601390@gmail.com>
2022-05-15 19:49:00 +0600

    array minimum number find

--- Practice_Day_9.c
+++ Practice_Day_9.c
@@ -1,12 +1,19 @@
 #include<stdio.h>
 
-void printText(char name[]) {
-    printf("Hello %s\n",name);
+int getMin(int a, int b){
+    return a < b ? a : b;
+}
+
+int getArrMin(int n, int a[]){
+    int min = a[0];
+    for(int i=0; i<n; i++)
+        min = getMin(a[i],min);
+    return min;
 }
 
 int main(){
-    printText("Md Rasel");
-    printText("Fi Rasel");
-
+    int sz = 7;
+    int a[] = {4, 3, 5, 7, 5, 2, 7};
+    printf("%d\n",getArrMin(sz,a));
     return 0;
 }
