ab25b48fe6b8b7aec77c148c10189517a2e76121
firasel <mdrasel601390@gmail.com>
2022-05-15 20:05:27 +0600

    function module complete

--- Practice_Day_9.c
+++ Practice_Day_9.c
@@ -1,19 +1,12 @@
 #include<stdio.h>
 
-int getMin(int a, int b){
-    return a < b ? a : b;
-}
-
-int getArrMin(int n, int a[]){
-    int min = a[0];
-    for(int i=0; i<n; i++)
-        min = getMin(a[i],min);
-    return min;
-}
-
 int main(){
-    int sz = 7;
-    int a[] = {4, 3, 5, 7, 5, 2, 7};
-    printf("%d\n",getArrMin(sz,a));
+      int fib[5];
+      fib[0] = fib[1] = 1;
+      for (int i=2; i<5; i++){
+        fib[i] = fib[i-1]+fib[i-2];
+      }
+      printf("%d", fib[4]);
+
     return 0;
 }
