57953c0ae24c6463e2499a1010b8f64bd404a6f0
firasel <mdrasel601390@gmail.com>
2022-05-26 16:24:17 +0600

    3 number problem solved

--- Practice_Day_14.c
+++ Practice_Day_14.c
@@ -1,30 +1,38 @@
 #include<stdio.h>
 
-int findNegatives(int n, int in[], int out[]){
-    int count = 0;
-    for(int i=0; i<n; i++){
-        if(in[i] < 0){
-            out[count] = in[i];
-            count++;
-        }
-    }
-    return count;
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void sort(int n, int a[]) {
+	for (int steps=0; steps<n; steps++) {
+    	int minelement = a[steps], pos = steps;
+    	for (int i=steps; i<n; i++) {
+        	if (a[i] < minelement) {
+            	minelement = a[i];
+            	pos = i;
+        	}
+    	}
+    	swap(&a[steps], &a[pos]);
+	}
 }
 
 int main() {
     int n;
     scanf("%d",&n);
 
-    int a[n],out[n];
+    int a[n];
     for(int i=0; i<n; i++)
         scanf("%d",&a[i]);
 
-    int totalFound = findNegatives(n,a,out);
-    printf("Total negative number is: %d\n",totalFound);
+    sort(n,a);
 
-    for(int i=0; i<totalFound; i++){
-        printf("%d ",out[i]);
+    for(int i=0; i<n; i++){
+        printf("%d ",a[i]);
     }
+
     printf("\n");
 
     return 0;
