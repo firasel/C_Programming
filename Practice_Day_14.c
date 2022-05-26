ca2f26e10eb11578d2cd70da9efdcd85f4e46925
firasel <mdrasel601390@gmail.com>
2022-05-26 16:51:06 +0600

    6 number problem solved

--- Practice_Day_14.c
+++ Practice_Day_14.c
@@ -1,16 +1,19 @@
 #include<stdio.h>
 #include<stdbool.h>
 
+static int count = 0;
+
 void swap(int *a, int *b){
     int temp = *a;
     *a = *b;
     *b = temp;
+    count++;
 }
 
 bool is_sorted(int n, int a[]){
     bool check = false;
     for(int i=0; i<n-1; i++){
-        if(a[i] > a[i+1]) check = true;
+        if(a[i] < a[i+1]) check = true;
         else{
             check = false;
             break;
@@ -19,10 +22,10 @@ bool is_sorted(int n, int a[]){
     return check;
 }
 
-void sort(int n, int a[]) {
+void bubbleSort(int n, int a[]) {
 	for (int steps=0; steps<n; steps++) {
     	for (int i=0; i+1<n; i++) {
-        	if (a[i] < a[i+1]) {
+        	if (a[i] > a[i+1]) {
             	swap(&a[i], &a[i+1]);
         	}
         }
@@ -30,15 +33,37 @@ void sort(int n, int a[]) {
 	}
 }
 
+void selectionSort(int n, int a[]) {
+	for (int steps=0; steps<n; steps++) {
+    	int minelement = a[steps], pos = steps;
+    	for (int i=steps; i<n; i++) {
+        	if (a[i] < minelement) {
+            	minelement = a[i];
+            	pos = i;
+        	}
+    	}
+    	swap(&a[steps], &a[pos]);
+    }
+}
+
 int main() {
     int n;
     scanf("%d",&n);
 
-    int a[n];
-    for(int i=0; i<n; i++)
+    int a[n],b[n];
+    for(int i=0; i<n; i++){
         scanf("%d",&a[i]);
+        b[i] = a[i];
+    }
 
-    sort(n,a);
+    bubbleSort(n,a);
+
+    printf("Bubble sort call the swap function of %d times.\n",count);
+    count = 0;
+
+    selectionSort(n,b);
+
+    printf("Selection sort call the swap function of %d times.\n",count);
 
     for(int i=0; i<n; i++){
         printf("%d ",a[i]);
