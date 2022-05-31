2cbf69158ea771e3e35653a19643c7acef64439d
firasel <mdrasel601390@gmail.com>
2022-06-01 00:25:55 +0600

    bit shifting

--- Practice_Day_17.c
+++ Practice_Day_17.c
@@ -1,7 +1,16 @@
 #include<stdio.h>
 #include<string.h>
 
-void toBinary(unsigned int val, char str[]){
+void strRev(char str[]){
+    int len = strlen(str) - 1;
+    for(int i=0; i<=len/2; i++){
+        char temp = str[i];
+        str[i] = str[len-i];
+        str[len-i] = temp;
+    }
+}
+
+void toBinary(unsigned int val, char str[], int k){
     int idx = 0;
     while(val > 0){
         int d = val%2;
@@ -10,20 +19,25 @@ void toBinary(unsigned int val, char str[]){
         idx++;
     }
 
-    str[idx] = '\0';
-
-    for(int i=0; i<idx/2; i++){
-        char temp = str[i];
-        str[i] = str[idx - i - 1];
-        str[idx - i - 1] = temp;
-    }
+    for(int i = idx; i<k; i++) str[i] = '0';
+    str[k] = 0;
+    strRev(str);
 }
 
 int main() {
-    char str[100];
-    unsigned int v = 10;
-    toBinary(v, str);
-    printf("%s\n",str);
+    unsigned int x = 10;
+    unsigned int y = 35;
+    char sx[100], sy[100], sand[100];
+    toBinary(x, sx, 8);
+    toBinary(y, sy, 8);
+
+    printf("sx = %s\n",sx);
+    printf("sy = %s\n",sy);
+
+    unsigned int and = x >> 2;
+    toBinary(and, sand, 8);
+    printf("and = %d\n",and);
+    printf("and = %s\n",sand);
 
     return 0;
 }
