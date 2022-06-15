23962d17fc590eb59875438858f084a0858b68d2
firasel <mdrasel601390@gmail.com>
2022-06-15 17:58:03 +0600

    practice day problem solved

---
+++ Practice_Day_24.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    int rMax=0,rMin=0;
+    char foodA[31]="", foodB[31]="";
+
+    for(int i=0; i<n; i++){
+        char food[31];
+        int r;
+        scanf("%s", food);
+        scanf(" %d", &r);
+        if(r > rMax){
+            rMax = r;
+            strcpy(foodA, food);
+        }
+        if(r < rMin){
+            rMin = r;
+            strcpy(foodB, food);
+        }
+    }
+    printf("%s %s\n", foodB, foodA);
+    return 0;
+}
