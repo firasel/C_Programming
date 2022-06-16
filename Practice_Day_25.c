03b3abe09fe64794137ce045a8e3971da17c2eee
firasel <mdrasel601390@gmail.com>
2022-06-16 17:54:57 +0600

    practice day problem 2 solved

--- Practice_Day_25.c
+++ Practice_Day_25.c
@@ -1,17 +1,12 @@
 #include<stdio.h>
 
-int get_sum(int arr[],int n){
-    int sum = 0;
-    while(n>0){
-        sum+=arr[n-1];
-        n--;
-    }
-    return sum;
+void print_oneToN(int n){
+    for(int i=1; i<=n;i++)
+        printf("%d ",i);
 }
 
 int main(){
-    int arr[] = {1,2,3,4,5};
-    printf("Sum is = %d",get_sum(arr,5));
+    print_oneToN(10);
 
     return 0;
 }
