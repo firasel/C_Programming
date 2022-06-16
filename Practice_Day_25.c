195c5b52b6fc8f30d4db10ece6dd34e2c86637ec
firasel <mdrasel601390@gmail.com>
2022-06-16 18:02:59 +0600

    practice day problem 3 solved

--- Practice_Day_25.c
+++ Practice_Day_25.c
@@ -1,12 +1,17 @@
 #include<stdio.h>
+#include<string.h>
 
-void print_oneToN(int n){
-    for(int i=1; i<=n;i++)
-        printf("%d ",i);
+int find_firstIndex(char str[],char aChar){
+    for(int i=0; i<strlen(str); i++){
+        if(str[i] == aChar)
+            return i;
+    }
+    return -1;
 }
 
 int main(){
-    print_oneToN(10);
+    int index = find_firstIndex("Hello world",'w');
+    index>=0 ? printf("Index number is = %d\n",index) : printf("Character not found!\n");
 
     return 0;
 }
