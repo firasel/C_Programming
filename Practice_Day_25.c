df291f2f4267a4d7814e695779151e6d1c3d8b2a
firasel <mdrasel601390@gmail.com>
2022-06-16 18:09:40 +0600

    practice day problem 5 solved

--- Practice_Day_25.c
+++ Practice_Day_25.c
@@ -1,17 +1,16 @@
 #include<stdio.h>
-#include<string.h>
 
-int find_lastIndex(char str[],char aChar){
-    for(int i=(strlen(str)-1); i>=0; i--){
-        if(str[i] == aChar)
-            return i;
-    }
-    return -1;
+void take_input(int arr[],int n){
+    for(int i=0; i<n; i++)
+        scanf("%d",&arr[i]);
 }
 
 int main(){
-    int index = find_lastIndex("Hello world",'l');
-    index>=0 ? printf("Last Index number is = %d\n",index) : printf("Character not found!\n");
+    int arr[5];
+    take_input(arr,5);
+
+    for(int i=0; i<5; i++)
+        printf("%d ",arr[i]);
 
     return 0;
 }
