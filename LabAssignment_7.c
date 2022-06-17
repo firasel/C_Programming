93876cc4fecb5f68a2931502f2890bc569d31f7a
firasel <mdrasel601390@gmail.com>
2022-06-17 17:35:17 +0600

    Assignment all question solved

--- LabAssignment_7.c
+++ LabAssignment_7.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
+void change_array(int arr[], int n, int arr2[]){
+    for(int i=0; i<n; i++)
+        arr2[i] = arr[n-i-1];
+}
 
 int main(){
-   int a=5;
-   int *p = &a;
-   int **q = &p;
-   int ***r = &q;
-   ***r = 10;
-   printf("%d\n",a);
-
-   return 0;
+    int arr[] = {1, 2, 3, 4, 5};
+    int arr2[5];
+    change_array(arr, 5, arr2);
+    for(int i=0; i<5; i++)
+        printf("%d ",arr2[i]);
+    return 0;
 }
