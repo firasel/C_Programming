43f612d9d0fd71d02022c00b3898ca68a8b3e549
firasel <mdrasel601390@gmail.com>
2022-06-25 01:47:25 +0600

    theory problem almost solved

--- LabAssignment_8.c
+++ LabAssignment_8.c
@@ -1,23 +1,6 @@
 #include<stdio.h>
-struct Person{
-    char s[22];
-    char t[22];
-};
-
 int main(){
-    int n;
-    scanf("%d",&n);
-    struct Person names[n];
-    for(int i=0; i<n; i++)
-        scanf("%s %s",&names[i].s, &names[i].t);
-    for(int i=0; i<n-1; i++){
-        for(int j=i+1; j<n; j++){
-            if(strcmp(names[i].s, names[j].s) == 0 && strcmp(names[i].t, names[j].t) == 0){
-                printf("Yes\n");
-                return 0;
-            }
-        }
-    }
-    printf("No\n");
+    for(int i=1; i<=100; i++)
+        i%2==0 ? printf("Goodbye\n") : printf("I Love Programming!\n");
     return 0;
 }
