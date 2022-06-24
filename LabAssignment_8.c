9644f47f2b46f97209f2f97bf4f7c43c13fe5a34
firasel <mdrasel601390@gmail.com>
2022-06-24 18:30:19 +0600

    E Number problem solved

--- LabAssignment_8.c
+++ LabAssignment_8.c
@@ -1,11 +1,31 @@
 #include<stdio.h>
-#include<string.h>
 int main(){
-    char str[52];
-    scanf("%s",&str);
-    int count=0;
-    for(int i=0; i<strlen(str); i++)
-        str[i]== 'a' && count++;
-    ((strlen(str)-count)<count) ? printf("%d\n",strlen(str)) : printf("%d\n",count*2-1);
+    int t=0;
+    scanf("%d",&t);
+    while(t>0){
+        int n,c;
+        scanf("%d",&n);
+        int num[n];
+        for(int i=0; i<n; i++)
+            scanf("%d",&num[i]);
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
+                if(num[i]>num[j]){
+                    int temp = num[j];
+                    num[j] = num[i];
+                    num[i] = temp;
+                }
+            }
+        }
+        for(int i=0; i<n-1; i++){
+            if(i == 0){
+                c = num[i+1] - num[i];
+            }else{
+                (num[i+1] - num[i] < c) && (c = (num[i+1] - num[i]));
+            }
+        }
+        printf("%d\n",c);
+        t--;
+    }
     return 0;
 }
