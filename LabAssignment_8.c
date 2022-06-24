97a36e902a885a2f52a386c02bd21ac6d81ce476
firasel <mdrasel601390@gmail.com>
2022-06-24 21:47:12 +0600

    all contest problem solved

--- LabAssignment_8.c
+++ LabAssignment_8.c
@@ -1,31 +1,23 @@
 #include<stdio.h>
+struct Person{
+    char s[22];
+    char t[22];
+};
+
 int main(){
-    int t=0;
-    scanf("%d",&t);
-    while(t>0){
-        int n,c;
-        scanf("%d",&n);
-        int num[n];
-        for(int i=0; i<n; i++)
-            scanf("%d",&num[i]);
-        for(int i=0; i<n-1; i++){
-            for(int j=i+1; j<n; j++){
-                if(num[i]>num[j]){
-                    int temp = num[j];
-                    num[j] = num[i];
-                    num[i] = temp;
-                }
+    int n;
+    scanf("%d",&n);
+    struct Person names[n];
+    for(int i=0; i<n; i++)
+        scanf("%s %s",&names[i].s, &names[i].t);
+    for(int i=0; i<n-1; i++){
+        for(int j=i+1; j<n; j++){
+            if(strcmp(names[i].s, names[j].s) == 0 && strcmp(names[i].t, names[j].t) == 0){
+                printf("Yes\n");
+                return 0;
             }
         }
-        for(int i=0; i<n-1; i++){
-            if(i == 0){
-                c = num[i+1] - num[i];
-            }else{
-                (num[i+1] - num[i] < c) && (c = (num[i+1] - num[i]));
-            }
-        }
-        printf("%d\n",c);
-        t--;
     }
+    printf("No\n");
     return 0;
 }
