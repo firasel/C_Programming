c7b73f20bcfab22d9e150d04920ddc1dc8c005c3
firasel <mdrasel601390@gmail.com>
2022-07-13 22:00:47 +0600

    HackerRank recursion function problem solve with C

--- Practice_Day_29.c
+++ Practice_Day_29.c
@@ -3,21 +3,21 @@
 #include <math.h>
 #include <stdlib.h>
 
-int main()
-{
+int find_nth_term(int n, int a, int b, int c) {
+    printf("%d\n",n);
+    if(n==1) return a;
+    else if(n==2) return b;
+    else if(n==3) return c;
 
-    int n;
-    scanf("%d", &n);
-    int len = n*2 - 1;
-    for(int i=0; i<len; i++){
-        for(int j=0; j<len; j++){
-            int min = i < j ? i : j;
-            min = min < len-i ? min : len-i-1;
-            min = min < len-j-1 ? min : len-j-1;
-            printf("%d ", n-min);
-        }
-        printf("\n");
-    }
+    return find_nth_term(n-1,a,b,c)+find_nth_term(n-2,a,b,c)+find_nth_term(n-3,a,b,c);
+}
 
+int main() {
+    int n, a, b, c;
+
+    scanf("%d %d %d %d", &n, &a, &b, &c);
+    int ans = find_nth_term(n, a, b, c);
+
+    printf("%d", ans);
     return 0;
 }
